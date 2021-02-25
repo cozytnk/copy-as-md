216fae825fcbb310512c8cca44c701bd7f9b68a7
cozy <cozy2027tnk@gmail.com>
2021-02-25 19:27:20 +0900

    Update background.js
    - show success / failure

--- background.js
+++ background.js
@@ -6,22 +6,28 @@ chrome.browserAction.onClicked.addListener(tab => {
   // [javascript - Copy to Clipboard in Chrome Extension - Stack Overflow](https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension/18455088#18455088)
 
   const copyTextToClipboard = text => {
-    const elem = document.createElement("textarea")
-
+    const elem = document.createElement('textarea')
     elem.textContent = text
-
     document.body.appendChild(elem)
-
     elem.select()
-
     document.execCommand('copy')
-
-    // (Optional) De-select the text using blur().
-    elem.blur()
-
+    elem.blur() // (Optional) De-select the text using blur().
     document.body.removeChild(elem)
   }
 
-  copyTextToClipboard(`[${tab.title}](${tab.url})`)
+  const setBadgeTextThenRemove = (text, ms) => {
+    chrome.browserAction.setBadgeText({ text }, () => {
+      setTimeout(() => {
+        chrome.browserAction.setBadgeText({ text: '' })
+      }, ms || 3000)
+    })
+  }
+
+  try {
+    copyTextToClipboard(`[${tab.title}](${tab.url})`)
+    setBadgeTextThenRemove('ok!', 2000)
+  } catch {
+    setBadgeTextThenRemove('ng...', 2000)
+  }
 })
 
