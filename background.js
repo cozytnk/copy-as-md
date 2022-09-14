a6dbf421604509efb4a128e0f026a4951e47d2da
cozytnk <cozy2027tnk@gmail.com>
2022-09-14 11:43:02 +0900

    refactor: v3対応

--- background.js
+++ background.js
@@ -1,37 +1,42 @@
-
-
-chrome.browserAction.setBadgeBackgroundColor({ color: '#444' })
-
-
-chrome.browserAction.onClicked.addListener(tab => {
-
-  // [javascript - How to get Clipboard data in Chrome Extension? - Stack Overflow](https://stackoverflow.com/questions/22702446/how-to-get-clipboard-data-in-chrome-extension)
-  // [javascript - Copy to Clipboard in Chrome Extension - Stack Overflow](https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension/18455088#18455088)
-
-  const copyTextToClipboard = text => {
-    const elem = document.createElement('textarea')
-    elem.textContent = text
-    document.body.appendChild(elem)
-    elem.select()
-    document.execCommand('copy')
-    elem.blur() // (Optional) De-select the text using blur().
-    document.body.removeChild(elem)
-  }
-
-  const setBadgeTextThenRemove = (text, ms) => {
-    chrome.browserAction.setBadgeText({ text }, () => {
-      setTimeout(() => {
-        chrome.browserAction.setBadgeText({ text: '' })
-      }, ms || 3000)
+const clipboard = {
+  writeText: async text => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: text => {
+        const el = document.createElement('textarea')
+        el.textContent = text
+        document.body.appendChild(el)
+        el.select()
+        document.execCommand('copy')
+        el.blur()
+        document.body.removeChild(el)
+        return
+      },
+      args: [text]
     })
+  }
+}
+
+
+chrome.action.onClicked.addListener(async tab => {
+  console.log('@action.onClicked', tab)
+
+  const setBadgeTextThenRemove = async (text, ms) => {
+    await chrome.action.setBadgeText({ tabId: tab.id, text })
+    setTimeout(() => {
+      chrome.action.setBadgeText({ tabId: tab.id, text: '' })
+    }, ms)
   }
 
   try {
-    const text = decodeURIComponent(`[${tab.title}](${tab.url})`)
-    copyTextToClipboard(text)
+    let text = decodeURIComponent(`[${tab.title}](${tab.url})`)
+    await clipboard.writeText(text)
     setBadgeTextThenRemove('ok!', 2000)
-  } catch {
-    setBadgeTextThenRemove('ng...', 2000)
+  } catch (error) {
+    setBadgeTextThenRemove('ng', 2000)
+    console.error(error)
   }
+
 })
 
